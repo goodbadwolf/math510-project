9ba11003374547f92ed2878e850679411550ff20
Manish Mathai <manishpmathai@gmail.com>
2017-11-24 18:27:17 -0800

    Spectrogram with frequencies and time values

--- src/project.m
+++ src/project.m
@@ -2,4 +2,4 @@
 [num_samples, num_channels] = size(audio);
 window_size_ms = 50.0;
 window_size_samples = floor((window_size_ms / 1000) * sample_rate);
-S = spectrogram(audio, window_size_samples);
+[S, W, T] = spectrogram(audio, window_size_samples, [], [], sample_rate);
