6164e5d8badcdf05ad15e3ccfa21f95377143faa
Manish Mathai <manishpmathai@gmail.com>
2017-11-24 17:50:46 -0800

    Get spectrogram

--- src/project.m
+++ src/project.m
@@ -1,2 +1,5 @@
-[audio, ~] = audioread('../timit/lisa/data/timit/raw/TIMIT/TRAIN/DR1/FCJF0/SA1.WAV');
+[audio, sample_rate] = audioread('../timit/lisa/data/timit/raw/TIMIT/TRAIN/DR1/FCJF0/SA1.WAV');
 [num_samples, num_channels] = size(audio);
+window_size_ms = 50.0;
+window_size_samples = floor((window_size_ms / 1000) * sample_rate);
+S = spectrogram(audio, window_size_samples);
